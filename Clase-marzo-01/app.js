63f0a224d50c70c87f1f8f3895580723e83c6470
joranvm1993 <66146001+joranvm1993@users.noreply.github.com>
2023-03-01 11:43:22 -0500

    Add files via upload

---
+++ Clase-marzo-01/app.js
@@ -0,0 +1,62 @@
+console.log('Hola Mundo');
+console.log(Math.random());
+
+let edad;
+let age = 25;
+
+let mensaje = "Esto es una prueba";
+var message = "Otra prueba";
+
+console.log(`${mensaje} ${message}`);
+
+console.log(message.length);
+
+const moneda = 'COP';
+
+
+
+
+const sumar = (numero1, numero2)=>{
+    return numero1 + numero2;
+}
+
+let resultado = sumar(3, 2);
+
+console.log(resultado);
+
+
+
+
+/////////////////////////////////////
+const multiplicar = () =>{
+    console.log(2*1);
+}
+
+multiplicar();
+/////////////////////////
+const mutiplicar1 = num =>{
+    return num * 5;
+}
+
+console.log(mutiplicar1(3));
+
+//////////////////////////////////
+
+const mutiplicar2 = (num1, num2)=>{
+    return num1 * num2;
+}
+
+console.log(mutiplicar2(5,6));
+
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+
+
+//////////////
+const adicionar = (numeros) =>{
+    return numeros.push(12);
+}
+
+adicionar(numeros);
+
+console.log(numeros);
+
